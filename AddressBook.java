b1b0613c1094ad40248352ee0a764e4242c117ad
Suyash Jain <jain.suyash28@gmail.com>
2020-09-30 00:08:39 +0530

    Added: New Data Addition Function to Address Book is Added

--- AddressBook.java
+++ AddressBook.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.util.*;
 public class AddressBook {
 	
 	public String firstName;		//Obj Attributes
@@ -43,18 +45,57 @@ public class AddressBook {
 
 	public static void main(String[] args) {
 		
+		ArrayList<AddressBook> record=new ArrayList<AddressBook>();
 		//Creating first entry
 		AddressBook entry1=new AddressBook("Suyash", "Jain",
 				"Najafgarh", "New Delhi", "Delhi", 110043, "9810224035",
 				"suyash.jain@capgemini.com");
+		record.add(entry1);					//Adding entry to record
 		System.out.println(entry1);
-		entry1.display();					//Method call to display details
-		
 		//Creating second entry
 		AddressBook entry2=new AddressBook("Harshit", "Jain",
 				"Mahaveer Nagar", "New Delhi", "Delhi", 110043, "8285683470",
 				"harshit.jain@gmail.com");
+		record.add(entry2);					//Adding entry to record
 		System.out.println(entry2);
-		entry2.display();					//Method call to display details		
+		
+		String firstName;					//Attributes to be added
+		String lastName;
+		String address;
+		String city;
+		String state;
+		long zipCode;
+		String phoneNo;
+		String email;
+		
+		//asking user input
+		System.out.println("Please enter details to be added.");
+		System.out.print("First Name: ");
+		firstName=sc.next();
+		System.out.print("Last Name: ");
+		lastName=sc.next();
+		System.out.print("Address: ");
+		address=sc.next();
+		System.out.print("City: ");
+		city=sc.next();
+		System.out.print("State: ");
+		state=sc.next();
+		System.out.print("ZipCode: ");
+		zipCode=sc.nextLong();
+		System.out.print("Phone No.: ");
+		phoneNo=sc.next();
+		System.out.print("Email: ");
+		email=sc.next();
+		
+		//saving as new entry
+		AddressBook entry3=new AddressBook(firstName,lastName,
+				address,city,state,zipCode,phoneNo,email);
+		record.add(entry3);					//Adding entry to record
+		System.out.println(entry3);
+		
+		//displaying all entries
+		entry1.display();
+		entry2.display();
+		entry3.display();
 	}
 }
