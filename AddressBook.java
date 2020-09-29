a5e5c1b362594bc82cdee0ec4a49a232d66f910d
Suyash Jain <jain.suyash28@gmail.com>
2020-09-30 00:52:23 +0530

    Refactored: Adding Multiple Address Book Function to Address Book is Added

--- AddressBook.java
+++ AddressBook.java
@@ -1,11 +1,14 @@
 import java.io.*;
 import java.util.*;
+
 public class AddressBook {
 	
-	private ArrayList<Collection> record;
+	private HashMap<String, ArrayList<Collection>> addressBook;
+	private static ArrayList<Collection> record;
 
 	public AddressBook() {
 		record=new ArrayList<Collection>();
+		addressBook = new HashMap<String, ArrayList<Collection>>();
 	}
 
 	public void display() {
@@ -13,12 +16,17 @@ public class AddressBook {
 			obj.display();
 		}
 	}
+
 	
 	public void addToRecord(Collection obj) {
 		record.add(new Collection(obj.firstName, obj.lastName, obj.address,
 				obj.city,obj.state, obj.zipCode, obj.phoneNo, obj.email));
 	}
 
+	public void addBookMap(String bookName) {
+		addressBook.put(bookName, new ArrayList<Collection>());
+	}
+	
 	public static Collection add() {
 		
 		//method for adding new entries.
@@ -57,6 +65,71 @@ public class AddressBook {
 		return entry;					//returning entry to main
 	}
 	
+	public static ArrayList<Collection> edit(ArrayList<Collection> list, String name) {
+		
+		//method for edit
+		Scanner sc=new Scanner(System.in);
+		boolean flag=false;
+		name.replaceAll("\\P{Print}","");
+		String lower_name=name.toLowerCase();
+		for (Collection obj:list) {
+			String firstName=obj.firstName.toLowerCase();
+			String lastName=obj.lastName.toLowerCase();
+			if (firstName.equals(lower_name) ||
+					lastName.equals(lower_name)) {
+				flag=true;
+				System.out.println("Please enter new details.");
+				System.out.print("Address: ");
+				obj.address=sc.next();
+				System.out.print("City: ");
+				obj.city=sc.next();
+				System.out.print("State: ");
+				obj.state=sc.next();
+				System.out.print("ZipCode: ");
+				obj.zipCode=sc.nextLong();
+				System.out.print("Phone No.: ");
+				obj.phoneNo=sc.next();
+				System.out.print("Email: ");
+				obj.email=sc.next();
+				
+				System.out.println("Record updated.");
+				break;
+			}
+		}
+		if (flag==false) {
+			System.out.println("No entry found for "+name);
+		}
+		return list;
+	}
+	
+	public static ArrayList<Collection> delete(ArrayList<Collection> list, String name) {
+		
+		//method for delete
+		Scanner sc=new Scanner(System.in);
+		boolean flag=false;
+		name.replaceAll("\\P{Print}","");
+		String lower_name=name.toLowerCase();
+		
+		try {
+			for (Collection obj:list) {
+				String firstName=obj.firstName.toLowerCase();
+				String lastName=obj.lastName.toLowerCase();
+				if (firstName.equals(lower_name) ||
+						lastName.equals(lower_name)) {
+					flag=true;
+					
+					System.out.println("Record deleted for "+obj.firstName+" "+obj.lastName);
+					list.remove(obj);			//delete entry from record
+					System.out.println("Record updated.");
+				}
+			}
+		}catch(Exception e) {}
+		if (flag==false) {
+			System.out.println("No entry found for "+name);
+		}
+		return list;
+	}
+	
 	public static void main(String[] args) {
 		
 		Scanner sc=new Scanner(System.in);
@@ -64,7 +137,7 @@ public class AddressBook {
 		
 		//Creating first entry
 		Collection entry1=new Collection("Suyash", "Jain",
-				"Najafgarh", "New Delhi", "Delhi", 110043, "981022035",
+				"Najafgarh", "New Delhi", "Delhi", 110043, "9810224035",
 				"suyash.jain@capgemini.com");
 		buildObj.addToRecord(entry1);				//Adding entry to record
 		System.out.println(entry1);
@@ -75,24 +148,67 @@ public class AddressBook {
 				"harshit.jain@gmail.com");
 		buildObj.addToRecord(entry2);				//Adding entry to record
 		System.out.println(entry2);
-
-		//initiating addition of entries
+		
+		buildObj.addBookMap("AddressBook1");
+		
+		//initiating user functions of entries
 		
 		String user_input="1";
-		while(user_input.equals("1")) {
+		while((user_input.equals("1") || user_input.equals("2") || user_input.equals("3"))) {
 			
-			Collection entry=buildObj.add();		//calling function to make new entry
-			buildObj.addToRecord(entry);			//Adding entry to record
-			System.out.println(entry);
+			// Checking in address list is present in hashmap
+			System.out.print("Enter the Name of the Address Book: ");
+			String bookName = sc.next();
+			if (buildObj.addressBook.containsKey(bookName)) {
+				buildObj.record = buildObj.addressBook.get(bookName);
+			}
+
+			else {
+				System.out.println("Address book with name " + bookName + " not found. Creating a new entry");
+				buildObj.addBookMap(bookName);
+			}
 			
-			//asking user to continue adding entries
-			System.out.println();
-			System.out.println("Press 1 to add new entry");
-			System.out.println("Press any other key to exit.");
+			System.out.println("Record "+bookName+" loaded.");
+			
+			System.out.println();				//Waiting for user input
+			System.out.println("What now?");
+			System.out.println("1. Add a new contact.");
+			System.out.println("2. Edit an existing contact.");
+			System.out.println("3. Delete an existing contact.");
+			System.out.println("4. Switch Directory");
+			System.out.println("Logout");
 			user_input=sc.next();
+			
+			switch(user_input) {
+			
+			case "1": {
+				Collection entry=buildObj.add();		//calling function to make new entry
+				buildObj.addToRecord(entry);			//Adding entry to record
+				System.out.println(entry);
+				break;
+			}
+			case "2": {
+				System.out.println("Please enter First/Last name of entry to be edited.");
+				String name=sc.next();
+				ArrayList<Collection> list = buildObj.edit(record,name);
+				break;
+			}
+			case "3": {
+				System.out.println("Please enter First/Last name of entry to be deleted.");
+				String name=sc.next();
+				record=delete(record,name);
+				break;
+			}
+			case "4": {
+				user_input="1";
+				continue;
+			}
+			default:
+				break;
+			}
 		}
 		
 		//displaying all entries
-			buildObj.display();
+		buildObj.display();
 	}
 }
